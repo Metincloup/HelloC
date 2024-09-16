c5464b98f5a3abff662a3bd3d915d77e2130f650
Metincloup <durmus38metin@gmail.com>
2024-09-16 21:12:48 +0300

    Comment Added

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[]) {
     srand(247607);
     for(int i = 0; i <= 4 ;i++){
-        printf("%c", rand() % 0x0B + 69);
+        printf("%c", rand() % 0x0B + 69); // 69 nice but it is really required. What a coincidence
     }
     printf("\n");
     return 0;
